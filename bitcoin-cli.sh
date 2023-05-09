7a93c77447e97d82fd25fcc95f246f1b5856f9c1
daGoodenough <dgoodenough@proton.me>
2023-05-09 13:39:21 -0700

    revert lightning-cli and bitcoin-cli scripts

--- bitcoin-cli.sh
+++ bitcoin-cli.sh
@@ -8,13 +8,7 @@ cd "$(dirname "$0")"
 
 if docker ps | grep -q bitcoind; then
     BITCOIND_CONTAINER_ID="$(docker ps | grep bitcoind | head -n1 | awk '{print $1;}')"
-    CMD="docker exec -t -u 1000:1000 $BITCOIND_CONTAINER_ID bitcoin-cli"
-    if [ "$BTC_CHAIN" != mainnet ]; then
-        CMD="$CMD -$BTC_CHAIN"
-    fi
-    
-    CMD="$CMD $*"
-    eval "$CMD"
+    docker exec -t -u 1000:1000 "$BITCOIND_CONTAINER_ID" bitcoin-cli -"$BTC_CHAIN" "$@"
 else
     echo "ERROR: Cannot find the bitcoind container. Did you run it?"
     exit 1
