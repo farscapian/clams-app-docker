cab7800756231ee922588771490c874ecae88487
Derek Smith <derek@farscapian.com>
2023-05-27 13:27:37 -0400

    Refactor the inital prompt.

--- up.sh
+++ up.sh
@@ -48,17 +48,8 @@ for i in "$@"; do
     esac
 done
 
-if [ "$ACTIVE_ENV" != "local.env" ] && [ "$USER_SAYS_YES" = false ]; then
-    read -p "WARNING: You are targeting something OTHER than a dev/local instance. Are you sure you want to continue? (yes/no): " answer
-
-    # Convert the answer to lowercase
-    ANSWER=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
-
-    # Check if the answer is "yes"
-    if [ "$ANSWER" != "yes" ]; then
-        echo "Quitting."
-        exit 1
-    fi
+if [ "$USER_SAYS_YES" = false ]; then
+    ./prompt.sh
 fi
 
 if [ "$ENABLE_TLS" = true ] && [ "$DOMAIN_NAME" = localhost ]; then
