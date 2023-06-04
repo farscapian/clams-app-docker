6139ae9461e56e3a50d38b415cc448a73b1f0adc
Derek Smith <derek@farscapian.com>
2023-06-04 16:08:27 -0400

    Update rune logic; add prismeditor.

--- get_rune.sh
+++ get_rune.sh
@@ -38,9 +38,27 @@ if [ "$RUNE_TYPE" = admin ]; then
         RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune")
     fi 
 elif [ "$RUNE_TYPE" = read-only ]; then
-    RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=waitanyinvoice\",\"method=waitinvoice\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
+    if [ -n "$SESSION_ID" ]; then
+        RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=waitanyinvoice\",\"method=waitinvoice\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
+    else
+        echo "ERROR: SESSION_ID not provided."
+        exit 1
+    fi
 elif [ "$RUNE_TYPE" = clams ]; then
-    RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=pay\",\"method=keysend\",\"method=invoice\",\"method=waitanyinvoice\",\"method=waitinvoice\", \"method=signmessage\", \"method^bkpr-\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
+    if [ -n "$SESSION_ID" ]; then
+        RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=pay\",\"method=keysend\",\"method=invoice\",\"method=waitanyinvoice\",\"method=waitinvoice\", \"method=signmessage\", \"method^bkpr-\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
+    else
+        echo "ERROR: SESSION_ID not provided."
+        exit 1
+    fi
+
+elif [ "$RUNE_TYPE" = prismeditor ]; then
+    RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"method/pay\"]]'")
+    #RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"method^list\",\"method^get\",\"method=summary\"],[\"method=listoffers\"],[\"method=offer\"],[\"method=datastore\"],[\"method=listdatastore\"],[\"method=createprism\"],[\"rate=60\"]]'")
+elif [ "$RUNE_TYPE" = prismreader ]; then
+#RUNE_JSON=$(bash -c "./lightning-cli.sh --id=${NODE_ID} commando-rune restrictions='[[\"id=$SESSION_ID\"], [\"method^list\",\"method^get\",\"method=summary\",\"method=pay\",\"method=keysend\",\"method=invoice\",\"method=waitanyinvoice\",\"method=waitinvoice\", \"method=signmessage\", \"method^bkpr-\"],[\"method/listdatastore\"], [\"rate=60\"]]'")
+echo "TODO"
+
 fi
 
 RUNE=$(echo "$RUNE_JSON" | jq -r '.rune')
