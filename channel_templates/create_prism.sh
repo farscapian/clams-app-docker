1bdf536d5d02b9d6c34a590c8f7bed917b719d2d
--get <derek@farscapian.com>
2023-06-16 16:35:28 -0400

    Change default prism name to 'ROYGBIV Demo'

--- channel_templates/create_prism.sh
+++ channel_templates/create_prism.sh
@@ -13,7 +13,7 @@ CAROL_PUBKEY=${pubkeys[2]}
 DAVE_PUBKEY=${pubkeys[3]}
 ERIN_PUBKEY=${pubkeys[4]}
 
-prism=$(lncli --id=1 createprism label="'"$RANDOM"'" members='[{"name":"carol", "destination": "'"$CAROL_PUBKEY"'", "split": 1}, {"name": "dave", "destination": "'"$DAVE_PUBKEY"'", "split": 5}, {"name": "erin", "destination": "'"$ERIN_PUBKEY"'", "split": 2}]' 2>&1)
+prism=$(lncli --id=1 createprism label="ROYGBIV Demo" members='[{"name":"carol", "destination": "'"$CAROL_PUBKEY"'", "split": 1}, {"name": "dave", "destination": "'"$DAVE_PUBKEY"'", "split": 5}, {"name": "erin", "destination": "'"$ERIN_PUBKEY"'", "split": 2}]' 2>&1)
 
 if [[ $? -ne 0 ]]; then
   echo "Error: Failed to create Prism offer"
