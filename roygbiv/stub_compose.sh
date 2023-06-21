01e1178bf339190f4145c2242e4866329f2ff4f9
--get <derek@farscapian.com>
2023-06-21 16:50:51 -0400

    Remove direct dependency on python.

--- roygbiv/stub_compose.sh
+++ roygbiv/stub_compose.sh
@@ -16,7 +16,14 @@ touch "$DOCKER_COMPOSE_YML_PATH"
 # let's generate a random username and password and get our -rpcauth=<token>
 BITCOIND_RPC_USERNAME=$(gpg --gen-random --armor 1 8 | tr -dc '[:alnum:]' | head -c10)
 BITCOIND_RPC_PASSWORD=$(gpg --gen-random --armor 1 32 | tr -dc '[:alnum:]' | head -c32)
-RPC_AUTH_TOKEN=$(./rpc-auth.py "$BITCOIND_RPC_USERNAME" "$BITCOIND_RPC_PASSWORD" | grep rpcauth)
+
+if ! docker image list | grep -q python; then
+    docker pull python
+fi
+
+RPC_AUTH_TOKEN=$(docker run -t -v ./scripts:/scripts python /scripts/rpc-auth.py "$BITCOIND_RPC_USERNAME" "$BITCOIND_RPC_PASSWORD" | grep rpcauth)
+RPC_AUTH_TOKEN="${RPC_AUTH_TOKEN//[$'\t\r\n ']}"
+echo "RPC_AUTH_TOKEN: $RPC_AUTH_TOKEN"
 
 BITCOIND_COMMAND="bitcoind -server=1 -${RPC_AUTH_TOKEN} -upnp=0 -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -rpcport=${BITCOIND_RPC_PORT:-18443} -rest -listen=1 -listenonion=0 -fallbackfee=0.0002 -mempoolfullrbf=1"
 
