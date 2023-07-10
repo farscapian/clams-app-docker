fa3249e8d4638b5e748a3a01709cbdef6110dd4f
Derek Smith <derek@farscapian.com>
2023-07-09 21:53:34 -0400

    Add load testing submodule from Aaron Barnard.

---
+++ run_load_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"
+
+. ./defaults.env
+. ./load_env.sh
+
+export DOCKER_HOST=
+
+OUTPUT_FILE="$(pwd)/output/cln_connection_info-${DOMAIN_NAME}.csv"
+
+if [ ! -f "$OUTPUT_FILE" ]; then
+    # ensure we have the correct input file containing connenction info
+    ./show_cln_uris.sh --output-file="$OUTPUT_FILE"
+fi
+
+docker system prune -f
+
+# next let's call the load testing script.
+./tests/load/run.sh --connection-csv-path="$OUTPUT_FILE"
