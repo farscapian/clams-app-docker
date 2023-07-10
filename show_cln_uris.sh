5ac7f7f0dab27584e28b2fa663af69f8852bbdb6
Derek Smith <derek@farscapian.com>
2023-07-09 23:09:23 -0400

    Add updates to show_cln_uris.

--- show_cln_uris.sh
+++ show_cln_uris.sh
@@ -4,6 +4,7 @@ set -e
 cd "$(dirname "$0")"
 
 PRODUCE_QR_CODE=false
+OUTPUT_FILE=
 
 # grab any modifications from the command line.
 for i in "$@"; do
@@ -12,14 +13,16 @@ for i in "$@"; do
             PRODUCE_QR_CODE=true
             shift
         ;;
+        --output-file=*)
+            OUTPUT_FILE="${i#*=}"
+            shift
+        ;;
         *)
         echo "Unexpected option: $1"
         exit 1
         ;;
     esac
 done
-
-mkdir -p ./output
 
 if [ "$PRODUCE_QR_CODE" = true ]; then
     if ! command -v qrencode >/dev/null 2>&1; then
@@ -30,16 +33,14 @@ if [ "$PRODUCE_QR_CODE" = true ]; then
     mkdir -p ./output/qrcodes
 fi
 
-
 . ./defaults.env
 . ./load_env.sh
 
+if [ -z "$OUTPUT_FILE" ]; then
+    OUTPUT_FILE="$(pwd)/output/cln_connection_info-${DOMAIN_NAME}.csv"""
+fi
+
 readarray -t names < ./roygbiv/names.txt
-
-OUTPUT_FILE="$(pwd)/output/cln_connection_info-${DOMAIN_NAME}.csv"
-printf "" > "$OUTPUT_FILE"
-
-echo "INFO: Writing direct links to $OUTPUT_FILE"
 
 # print out the CLN node URIs for the user.
 for (( CLN_ID=0; CLN_ID<CLN_COUNT; CLN_ID++ )); do
@@ -55,22 +56,41 @@ for (( CLN_ID=0; CLN_ID<CLN_COUNT; CLN_ID++ )); do
     #     CLN_P2P_PORT="$CLN_P2P_PORT_OVERRIDE"
     # fi
 
-    #CLN_P2P_URI=$(bash -c "./get_node_uri.sh --id=${CLN_ID} --port=${CLN_P2P_PORT}")
+    if [ "$BTC_CHAIN" = regtest ]; then
+        CLN_P2P_URI=$(bash -c "./get_node_uri.sh --id=${CLN_ID} --port=9735 --internal-only")
+    else
+        CLN_P2P_URI=$(bash -c "./get_node_uri.sh --id=${CLN_ID} --port=9735")
+    fi
 
     # now let's output the core lightning node URI so the user doesn't need to fetch that manually.
     CLN_WEBSOCKET_URI=$(bash -c "./get_node_uri.sh --id=${CLN_ID} --port=${CLN_WEBSOCKET_PORT}")
 
     echo "  websocket_uri: $CLN_WEBSOCKET_URI"
+    echo "  node_uri: $CLN_P2P_URI"
     PROTOCOL="ws:"
     if [ "$ENABLE_TLS" = true ]; then 
         PROTOCOL="wss:"
     fi
 
+    HTTP_PROTOCOL="http"
+    if [ "$ENABLE_TLS" = true ]; then 
+        HTTP_PROTOCOL="https"
+    fi
+
     RUNE=$(bash -c "./get_rune.sh --id=${CLN_ID} --admin")
     echo "  admin_rune: $RUNE"
     WEBSOCKET_QUERY_STRING="https://staging.app.clams.tech/connect?address=$CLN_WEBSOCKET_URI&type=direct&value=$PROTOCOL&rune=$RUNE"
-    echo "$WEBSOCKET_QUERY_STRING" >> "$OUTPUT_FILE"
 
+    # if the output file is specified, write out the query string
+    if [ -n "$OUTPUT_FILE" ]; then
+        if [ "$CLN_ID" = 0 ]; then
+            echo "$WEBSOCKET_QUERY_STRING" > "$OUTPUT_FILE"
+        else
+            echo "$WEBSOCKET_QUERY_STRING" >> "$OUTPUT_FILE"
+        fi
+    fi
+
+    echo "  direct_link: $WEBSOCKET_QUERY_STRING"
     if [ "$PRODUCE_QR_CODE" = true ]; then
         qrencode -o "$(pwd)/output/qrcodes/${DOMAIN_NAME}_cln-${CLN_ID}_websocket.png" -t png "$WEBSOCKET_QUERY_STRING"
     fi
