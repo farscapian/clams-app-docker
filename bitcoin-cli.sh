4d3269d9e7892c6108f9e167751c7c8f96388ceb
Derek Smith <derek@farscapian.com>
2023-07-23 20:34:44 -0400

    Fix bitcoin-cli bug.

--- bitcoin-cli.sh
+++ bitcoin-cli.sh
@@ -9,10 +9,10 @@ cd "$(dirname "$0")"
 BITCOIND_CONTAINER_ID="$(docker ps | grep 'roygbiv-stack_bitcoind.' | head -n1 | awk '{print $1;}')"
 if [ -n "$BITCOIND_CONTAINER_ID" ]; then
     if [ "$BTC_CHAIN" = mainnet ]; then
-        docker exec -t -u 1000:1000 "$BITCOIND_CONTAINER_ID" bitcoin-cli "$@"
+        docker exec -t -u 1000:1000 "$BITCOIND_CONTAINER_ID" bitcoin-cli -rpcport=18443 "$@"
     else
         # for signet and regtest we have to pass the -CHAIN param
-        docker exec -t -u 1000:1000 "$BITCOIND_CONTAINER_ID" bitcoin-cli -"$BTC_CHAIN" "$@"
+        docker exec -t -u 1000:1000 "$BITCOIND_CONTAINER_ID" bitcoin-cli -rpcport=18443 -"$BTC_CHAIN" "$@"
     fi
 else
     echo "ERROR: Cannot find the bitcoind container."
