4c9c8041838d463dd075457aab8431b5e3139319
Derek Smith <derek@farscapian.com>
2023-09-07 16:12:04 -0400

    Updates

--- lnplay/clightning/cln-plugins/lnplaylive/provision_lxd.sh
+++ lnplay/clightning/cln-plugins/lnplaylive/provision_lxd.sh
@@ -2,4 +2,22 @@
 
 set -exu
 
-lxc remote list
+if ! lxc remote list | grep -q lnplaylive; then
+    lxc remote add lnplaylive "$LNPLAY_LXD_FQDN_PORT" --password "$LNPLAY_LXD_PASSWORD" --accept-certificate
+fi
+
+if ! lxc remote get-default | grep -q lnplaylive; then
+    lxc remote switch lnplaylive
+fi
+
+if ! lxc project list | grep -q lnplaylivemvp; then
+    lxc project create lnplaylivemvp
+fi
+
+if ! lxc project list | grep -q "default (current)"; then
+    lxc project switch lnplaylivemvp
+fi
+
+cd /sovereign-stack
+
+#bash -c "$(pwd)/deployment/up.sh"
