03876b9d837453f343b73bd802f1fb2d77e36b23
Derek Smith <derek@farscapian.com>
2023-09-11 18:07:39 -0400

    Refactor lnplay.live frontend build process.

--- lnplay/lnplaylive/build.sh
+++ lnplay/lnplaylive/build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-set -eu
+set -exu
 cd "$(dirname "$0")"
 
 # before I can do any of this, I need to stub out the .env file...
 # in order to do that, the lightning nodes need to be up first.
 LNPLAY_LIVE_ENV="$(pwd)/app/.env"
-
 PUBLIC_ADDRESS="$(bash -c "../../get_node_uri.sh --id=1 --port=6002")"
-PUBLIC_RUNE="$(bash -c "../../get_rune.sh --id=1 --lnplaylive")"
 PUBLIC_WEBSOCKET_PROXY="$(echo "$PUBLIC_ADDRESS" | grep -o '@.*')"
+
 WS_PROTO=ws
 if [ "$ENABLE_TLS" = true ]; then
     WS_PROTO=wss
 fi
+
+PUBLIC_RUNE=$(bash -c "../../lightning-cli.sh --id=1 commando-rune restrictions='[[\"method^lnplaylive\",\"method=waitinvoice\",\"rate=120\"]]'"  | jq -r '.rune')
 
 cat > "$LNPLAY_LIVE_ENV" <<EOF
 PUBLIC_ADDRESS="${PUBLIC_ADDRESS}"
