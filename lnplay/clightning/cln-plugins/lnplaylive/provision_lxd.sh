a683f12799205f416c1d9dd2eb73571945803aa4
Derek Smith <derek@farscapian.com>
2023-09-14 09:43:46 -0400

    Stub out remote.conf on provisioning script.

--- lnplay/clightning/cln-plugins/lnplaylive/provision_lxd.sh
+++ lnplay/clightning/cln-plugins/lnplaylive/provision_lxd.sh
@@ -10,14 +10,32 @@ if ! lxc remote get-default | grep -q lnplaylive; then
     lxc remote switch lnplaylive
 fi
 
-if ! lxc project list | grep -q lnplaylivemvp; then
-    lxc project create lnplaylivemvp
+if ! lxc project list | grep -q lnplaylive; then
+    lxc project create lnplaylive
 fi
 
 if ! lxc project list | grep -q "default (current)"; then
-    lxc project switch lnplaylivemvp
+    lxc project switch lnplaylive
 fi
 
 cd /sovereign-stack
 
+REMOTE_CONF_PATH="$HOME/ss/remotes/$(lxc remote get-default)"
+mkdir -p "$REMOTE_CONF_PATH"
+
+REMOTE_CONF_FILE_PATH="$REMOTE_CONF_PATH/remote.conf"
+# need to get the remote.conf in there
+cat > "$REMOTE_CONF_FILE_PATH" <<EOF
+LXD_REMOTE_PASSWORD=
+DEPLOYMENT_STRING=
+# REGISTRY_URL=http://registry.domain.tld:5000
+EOF
+
+# need to get the project.conf in there
+
+# need to get the site.conf in there
+
+
 #bash -c "$(pwd)/deployment/up.sh"
+
+lxc project switch default
