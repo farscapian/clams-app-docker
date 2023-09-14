b7bbbdb75e8759291c0464e08531dcee684ba0e6
Derek Smith <derek@farscapian.com>
2023-09-14 12:03:40 -0400

    lnplaylive provisioning script updates.

--- lnplay/clightning/cln-plugins/lnplaylive/provision_lxd.sh
+++ lnplay/clightning/cln-plugins/lnplaylive/provision_lxd.sh
@@ -3,22 +3,23 @@
 set -exu
 
 if ! lxc remote list | grep -q lnplaylive; then
-    lxc remote add lnplaylive "$LNPLAY_LXD_FQDN_PORT" --password "$LNPLAY_LXD_PASSWORD" --accept-certificate
+    lxc remote add lnplaylive -q "$LNPLAY_LXD_FQDN_PORT" --password "$LNPLAY_LXD_PASSWORD" --accept-certificate > /dev/null
 fi
 
 if ! lxc remote get-default | grep -q lnplaylive; then
     lxc remote switch lnplaylive
+else
+    echo "WARNING: The lxc remote WAS NOT set to local. This could mean a prior deployment left the system in an odd state."
 fi
 
-if ! lxc project list | grep -q lnplaylive; then
-    lxc project create lnplaylive
+PROJECT_NAME="LABEL_XXX-EXPIRATION_DATE_UNIX"
+if ! lxc project list | grep -q "$PROJECT_NAME"; then
+    lxc project create "$PROJECT_NAME"
 fi
 
-if ! lxc project list | grep -q "default (current)"; then
-    lxc project switch lnplaylive
+if ! lxc project list | grep -q "$PROJECT_NAME (current)"; then
+    lxc project switch "$PROJECT_NAME"
 fi
-
-cd /sovereign-stack
 
 REMOTE_CONF_PATH="$HOME/ss/remotes/$(lxc remote get-default)"
 mkdir -p "$REMOTE_CONF_PATH"
@@ -32,10 +33,28 @@ DEPLOYMENT_STRING=
 EOF
 
 # need to get the project.conf in there
+PROJECT_CONF_PATH="$REMOTE_CONF_PATH/projects/$PROJECT_NAME"
+mkdir -p "$PROJECT_CONF_PATH"
+
+PROJECT_CONF_FILE_PATH="$PROJECT_CONF_PATH/project.conf"
+
+# todo, there needs to be some database/file of mac_addresses that can be used.
+export VM_MAC_ADDRESS="MAC_ADDRESS"
+cat > "$PROJECT_CONF_FILE_PATH" <<EOF
+PRIMARY_DOMAIN="${DOMAIN_NAME}"
+LNPLAY_SERVER_MAC_ADDRESS=${VM_MAC_ADDRESS}
+# LNPLAY_SERVER_CPU_COUNT="4"
+# LNPLAY_SERVER_MEMORY_MB="4096"
+EOF
 
 # need to get the site.conf in there
+cd /sovereign-stack
 
 
-#bash -c "$(pwd)/deployment/up.sh"
+#./deployment/up.sh
 
+# set the project to default
 lxc project switch default
+
+# set the remote to local.
+lxc remote switch local
