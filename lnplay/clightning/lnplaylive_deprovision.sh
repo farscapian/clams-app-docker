8041a3b1607ded5a521b6e1ae6565eb4eb41e3b6
Derek Smith <derek@farscapian.com>
2023-10-19 17:05:30 -0400

    Refactor deprovision script to container cron

---
+++ lnplay/clightning/lnplaylive_deprovision.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -exu
+
+# Now let's clean up all the projects from the cluster.
+PROJECT_NAMES=$(lxc project list --format csv -q | grep -vw default | cut -d',' -f1)
+
+# Iterate over each project name
+for OLD_PROJECT_NAME in $PROJECT_NAMES; do
+    if ! echo "$OLD_PROJECT_NAME" | grep -q default; then
+        if ! echo "$OLD_PROJECT_NAME" | grep -q current; then
+
+            SLOT="$OLD_PROJECT_NAME"
+            export SLOT="$SLOT"
+
+            source /dev-plugins/lnplaylive/lnplaylive.sh
+
+            lxc project switch "$SLOT"
+
+            INSTANCE=$(lxc list --format csv -q --columns n)
+            if [ -n "$INSTANCE" ]; then
+                lxc delete -f "$INSTANCE"
+            fi
+
+            PROFILE=$(lxc profile list -q --format csv | grep -v "default," | cut -d',' -f1)
+            if [ -n "$PROFILE" ]; then
+                lxc profile delete "$PROFILE"
+            fi
+
+            lxc project switch default >> /dev/null
+            ssh-keygen -R "${INSTANCE//-/.}" "$HOME/.ssh/known_hosts"
+            lxc project delete "$SLOT" >> /dev/null
+        fi
+    fi
+done
+
+rm -rf "$HOME/ss"
+
+# set the project to default
+lxc project switch default > /dev/null
+
+echo "" > "$HOME/.ssh/known_hosts"
