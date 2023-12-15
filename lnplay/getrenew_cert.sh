9384f74c3a4a088a57d8aba13ab58f1cd843764d
Derek Smith <derek@farscapian.com>
2023-12-15 16:44:51 -0500

    Only pull image when needed.

--- lnplay/getrenew_cert.sh
+++ lnplay/getrenew_cert.sh
@@ -8,7 +8,9 @@ docker volume create lnplay-certs >> /dev/null
 
 # let's do a refresh of the certificates. Let's Encrypt will not run if it's not time.
 CERTBOT_IMAGE_NAME="certbot/certbot:latest"
-docker pull -q "$CERTBOT_IMAGE_NAME" >> /dev/null
+if ! docker image inspect "$CERTBOT_IMAGE_NAME" &> /dev/null; then
+    docker pull -q "$CERTBOT_IMAGE_NAME" >> /dev/null
+fi
 
 GET_CERT_STRING="docker run -t --rm --name certbot -p 80:80 -p 443:443 -v lnplay-certs:/etc/letsencrypt ${CERTBOT_IMAGE_NAME} certonly -v --noninteractive --agree-tos --key-type ecdsa --standalone --expand -d ${DOMAIN_NAME} --email info@${DOMAIN_NAME}"
 
