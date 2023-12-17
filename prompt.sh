178d03c01dbb17e8fe5b9fd47040a79758dc4075
Derek Smith <derek@farscapian.com>
2023-12-17 16:48:32 -0500

    Nitpicks

--- prompt.sh
+++ prompt.sh
@@ -3,7 +3,7 @@
 set -exu
 
 if [ "$DOMAIN_NAME" != "127.0.0.1" ]; then
-    read -r -p "WARNING: You are targeting something OTHER than a dev/local instance: $DOMAIN_NAME Are you sure you want to continue? (yes/no): " ANSWSER
+    read -r -p "WARNING: You are targeting a remote LNPlay instance at '$DOMAIN_NAME'. Are you sure you want to continue? (yes/no): " ANSWSER
 
     # Convert the answer to lowercase
     ANSWER=$(echo "$ANSWSER" | tr '[:upper:]' '[:lower:]')
