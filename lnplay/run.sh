e0e2aaae501a4d85021b495ea18ad879b7f167bb
Derek Smith <derek@farscapian.com>
2024-01-07 15:03:28 -0500

    Convert to pseud-term

--- lnplay/run.sh
+++ lnplay/run.sh
@@ -33,7 +33,7 @@ export BITCOIND_DOCKER_IMAGE_NAME="$BITCOIND_DOCKER_IMAGE_NAME"
 
 if ! docker image inspect "$BITCOIND_DOCKER_IMAGE_NAME" &>/dev/null; then
     # build custom bitcoind image
-    docker build -t "$BITCOIND_DOCKER_IMAGE_NAME" --build-arg BASE_IMAGE="${BITCOIND_BASE_IMAGE_NAME}" ./bitcoind/
+    script -q -c "docker build -t $BITCOIND_DOCKER_IMAGE_NAME --build-arg BASE_IMAGE=${BITCOIND_BASE_IMAGE_NAME} ./bitcoind/"
 fi
 
 BITCOIND_MANAGER_IMAGE_NAME="lnplay/manager:$LNPLAY_STACK_VERSION"
