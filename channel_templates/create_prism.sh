99df11e55897bc67589a009f76ed60652648eb84
Derek Smith <derek@farscapian.com>
2024-04-28 15:27:19 -0500

    Update lnplay/bolt12-prism test script.

--- channel_templates/create_prism.sh
+++ channel_templates/create_prism.sh
@@ -31,42 +31,41 @@ PRISM_JSON_STRING="${PRISM_JSON_STRING::-1}]"
 
 # let's create 3 prisms
 ../lightning-cli.sh --id=1 prism-create -k prism_id="prism1" members="$PRISM_JSON_STRING"
-# ../lightning-cli.sh --id=1 prism-create -k prism_id="prism2" members="$PRISM_JSON_STRING" 
-# ../lightning-cli.sh --id=1 prism-create -k prism_id="prism3" members="$PRISM_JSON_STRING"
+../lightning-cli.sh --id=1 prism-create -k prism_id="prism2" members="$PRISM_JSON_STRING" 
+../lightning-cli.sh --id=1 prism-create -k prism_id="prism3" members="$PRISM_JSON_STRING"
 
 
 # now let's create offers
 OFFER_ID_A=$(../lightning-cli.sh --id=1 offer -k amount=any description="prism1_offer" label="prism1_offer" | jq -r '.offer_id')
-# OFFER_ID_B=$(../lightning-cli.sh --id=1 offer -k amount=any description="offer_b" label="offer_b" | jq -r '.offer_id')
-# OFFER_ID_C=$(../lightning-cli.sh --id=1 offer -k amount=any description="offer_c" label="offer_c" | jq -r '.offer_id')
+OFFER_ID_B=$(../lightning-cli.sh --id=1 offer -k amount=any description="offer_b" label="offer_b" | jq -r '.offer_id')
+OFFER_ID_C=$(../lightning-cli.sh --id=1 offer -k amount=any description="offer_c" label="offer_c" | jq -r '.offer_id')
 
 # now lets bind prism1 to prism1_offer. This is valid.
 ../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism1" bind_to="$OFFER_ID_A"
 
-# sleep 5
-# # binding again simply replaces the prism given a distinct offer.
-# ../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism2" bind_to="$OFFER_ID_A"
+# binding again simply replaces the prism given a distinct offer.
+../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism2" bind_to="$OFFER_ID_A"
 
-# # ok let's bind prism1 to offer_b. This is valid.
-# ../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism1" bind_to="$OFFER_ID_B"
+# ok let's bind prism1 to offer_b. This is valid.
+../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism1" bind_to="$OFFER_ID_B"
 
-# # Let's just add another typical binding.
-# ../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism3" bind_to="$OFFER_ID_C"
+# Let's just add another typical binding.
+../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism3" bind_to="$OFFER_ID_C"
 
 
-# # ok, so now let's execute some manual payouts to ensure the splits are working.
+# ok, so now let's execute some manual payouts to ensure the splits are working.
 
-# # 10k sats
-# # test prism.pay
-# AMOUNT_TO_PAY_MSAT="10000000"
-# ../lightning-cli.sh --id=1 prism-pay -k prism_id="prism1" amount_msat="$AMOUNT_TO_PAY_MSAT"
-# sleep 1
-# ../lightning-cli.sh --id=1 prism-pay -k prism_id="prism2" amount_msat="$AMOUNT_TO_PAY_MSAT"
-# sleep 1
-# ../lightning-cli.sh --id=1 prism-pay -k prism_id="prism3" amount_msat="$AMOUNT_TO_PAY_MSAT"
+# 10k sats
+# test prism.pay
+AMOUNT_TO_PAY_MSAT="10000000"
+../lightning-cli.sh --id=1 prism-pay -k prism_id="prism1" amount_msat="$AMOUNT_TO_PAY_MSAT"
+sleep 1
+../lightning-cli.sh --id=1 prism-pay -k prism_id="prism2" amount_msat="$AMOUNT_TO_PAY_MSAT"
+sleep 1
+../lightning-cli.sh --id=1 prism-pay -k prism_id="prism3" amount_msat="$AMOUNT_TO_PAY_MSAT"
 
-# # now let's create a bolt11 invoice and bind prism2 to it.
-# BOLT11_INVOICE_LABEL="BOLT11-001"
-# ../lightning-cli.sh --id=1 invoice -k amount_msat="$AMOUNT_TO_PAY_MSAT" label="$BOLT11_INVOICE_LABEL" description="test123"
-# sleep 1
-# ../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism2" bind_to="$BOLT11_INVOICE_LABEL" bolt_version="bolt11"
+# now let's create a bolt11 invoice and bind prism2 to it.
+BOLT11_INVOICE_LABEL="BOLT11-001"
+../lightning-cli.sh --id=1 invoice -k amount_msat="$AMOUNT_TO_PAY_MSAT" label="$BOLT11_INVOICE_LABEL" description="test123"
+sleep 1
+../lightning-cli.sh --id=1 prism-bindingadd -k prism_id="prism2" bind_to="$BOLT11_INVOICE_LABEL" bolt_version="bolt11"
