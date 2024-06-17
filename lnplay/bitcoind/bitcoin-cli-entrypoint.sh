582aa3e75640f89573700eca78e485aa53ceffe8
Derek Smith <derek@farscapian.com>
2024-06-17 10:38:09 -0400

    Fix mainnet cookie handling in bitcoin-cli.sh

--- lnplay/bitcoind/bitcoin-cli-entrypoint.sh
+++ lnplay/bitcoind/bitcoin-cli-entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -eu
 
-COOKIE_FILE_CONTENT=$(cat /bitcoind-cookie/.cookie)
+COOKIE_FILE=/bitcoind-cookie/.cookie
+COOKIE_FILE_CONTENT=$(cat "$COOKIE_FILE")
+if [ ! -f "$COOKIE_FILE" ]; then
+    echo "ERROR: Cannot find the authentication cookie."
+    sleep 5
+    exit 1
+fi
+
 BITCOIND_RPC_USERNAME=$(echo "$COOKIE_FILE_CONTENT" | cut -d':' -f1)
 BITCOIND_RPC_PASSWORD=$(echo "$COOKIE_FILE_CONTENT" | cut -d':' -f2)
 export BITCOIND_RPC_USERNAME="$BITCOIND_RPC_USERNAME"
 export BITCOIND_RPC_PASSWORD="$BITCOIND_RPC_PASSWORD"
 
-exec bitcoin-cli -rpcport=18443 -rpcuser="$BITCOIND_RPC_USERNAME" -rpcpassword="$BITCOIND_RPC_PASSWORD" -"${BTC_CHAIN}" "$@"
+if [ "$BTC_CHAIN" != "mainnet" ]; then
+    exec bitcoin-cli -rpcport=18443 -rpcuser="$BITCOIND_RPC_USERNAME" -rpcpassword="$BITCOIND_RPC_PASSWORD" -"${BTC_CHAIN}" "$@"
+else
+    exec bitcoin-cli -rpcport=18443 -rpcuser="$BITCOIND_RPC_USERNAME" -rpcpassword="$BITCOIND_RPC_PASSWORD" "$@"
+fi
